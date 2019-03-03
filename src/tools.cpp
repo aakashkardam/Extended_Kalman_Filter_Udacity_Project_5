2a848879f83bb536f0b61cfab914497a3940947c
aakashkardam <aakashkardam94@gmail.com>
2019-03-03 18:33:56 -0500

    updated CalculateJacobian function in tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,7 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::endl;
 
 Tools::Tools() {}
 
@@ -49,5 +50,30 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+ 
+  MatrixXd Hj(3, 4);
+  //recover state parameters
+  double px = x_state(0);
+  double py = x_state(1);
+  double vx = x_state(2);
+  double vy = x_state(3);
+
+  //pre-compute a set of terms to avoid repeated calculation
+  double c1 = px*px + py*py;
+  double c2 = sqrt(c1);
+  double c3 = (c1*c2);
+
+  //check division by zero
+  if (fabs(c1) < 0.0001){
+      std::cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+      return Hj;
+  }
+
+  //compute the Jacobian matrix
+  Hj << (px/c2), (py/c2), 0, 0,
+      -(py/c1), (px/c1), 0, 0,
+      py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+  return Hj;
   
 }
