bcdc20406b89131922107dfcd0254327cdad8f44
aakashkardam <aakashkardam94@gmail.com>
2019-03-03 18:00:33 -0500

    updated kalman filter 'updateEKF' function

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -52,4 +52,32 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  double px, py, vx, vy;
+  px = x_(0); // position in x
+  py = x_(1); // position in y
+  vx = x_(2); // velocity in x
+  vy = x_(3); // velocity in y
+
+  double rho = sqrt(px * px + py * py);
+  double phi = atan2(py, px);
+  double rho_dot;
+
+  if (fabs(rho) < 0.0001) { // checking division by 0
+    rho_dot = 0;
+  } else {
+    rho_dot = (px*vx + py*vy)/rho;
+  }
+
+  VectorXd h(3);
+  h << rho, phi, rho_dot;
+  VectorXd y = z - h;             // comparing the measurement with the prediction
+  MatrixXd Ht = H_.transpose();   // taking transpose of H
+  MatrixXd S = H_ * P_ * Ht + R_; // projecting measurement uncertainty into the meaurement space
+  MatrixXd Si = S.inverse();      // taking inverse of S
+  MatrixXd K =  P_ * Ht * Si;     // Kalman Gain 
+  
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;         // update our estimate and uncertainty
 }
